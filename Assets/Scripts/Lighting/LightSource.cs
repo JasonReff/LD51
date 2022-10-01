fd660f73b1dbb542fc9e94f2162b64071b19422a
JasonReff <jmr8382@rit.edu>
2022-10-01 10:40:34 -0400

    Toggle light prefab

--- Assets/Scripts/Lighting/LightSource.cs
+++ Assets/Scripts/Lighting/LightSource.cs
@@ -1,20 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class LightSource : MonoBehaviour
 {
+    private List<ContactPoint2D> contactPoints = new List<ContactPoint2D>();
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<ToggleLight>(out ToggleLight toggleLight))
+        if (collision.TryGetComponent<ToggleLight>(out ToggleLight lightToggle))
         {
-            toggleLight.ChangeVisibility(true);
+            lightToggle.ChangeVisibility(true);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<ToggleLight>(out ToggleLight toggleLight))
+        if (collision.TryGetComponent<ToggleLight>(out ToggleLight lightToggle))
         {
-            toggleLight.ChangeVisibility(false);
+            lightToggle.ChangeVisibility(false);
         }
     }
 }
--- Assets/Scripts/Lighting/ToggleLight.cs
+++ Assets/Scripts/Lighting/ToggleLight.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class ToggleLight : MonoBehaviour
 {
 
