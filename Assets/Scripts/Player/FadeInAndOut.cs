28a3dde1aad7f72c0ed3447e5e4da7debe6febec
JasonReff <jmr8382@rit.edu>
2022-10-02 18:13:54 -0400

    fog scroll

---
+++ Assets/Scripts/Player/FadeInAndOut.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInAndOut : MonoBehaviour
+{
+    [SerializeField] private float _minAlpha, _maxAlpha, _fadeRate;
+
+    private IEnumerator Start()
+    {
+        var rawImage = GetComponent<RawImage>();
+        while (true)
+        {
+            rawImage.DOFade(_minAlpha, _fadeRate);
+            yield return new WaitForSeconds(_fadeRate);
+            rawImage.DOFade(_maxAlpha, _fadeRate);
+            yield return new WaitForSeconds(_fadeRate);
+        }
+    }
+}
