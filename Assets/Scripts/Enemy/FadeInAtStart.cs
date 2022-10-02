ee7fa410a95dd43926bc1b86953527a2654fef48
JasonReff <jmr8382@rit.edu>
2022-10-01 23:02:53 -0400

    Ghost Changes

---
+++ Assets/Scripts/Enemy/FadeInAtStart.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class FadeInAtStart : MonoBehaviour
+{
+    [SerializeField] private float _fadeDuration;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().DOFade(1f, _fadeDuration);
+    }
+}
