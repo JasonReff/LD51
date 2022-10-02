7d896dca1a3ef9ef7899c29febf62980b3f1243a
JasonReff <jmr8382@rit.edu>
2022-10-01 23:06:20 -0400

    Fixed scene door

--- Assets/Scripts/Environment/NextSceneDoor.cs
+++ Assets/Scripts/Environment/NextSceneDoor.cs
@@ -5,25 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class NextSceneDoor : MonoBehaviour
 {
-<<<<<<< Updated upstream:Assets/Scripts/Environment/NextSceneDoor.cs
     [SerializeField] private string _sceneName;
     public void MoveToScene()
-=======
-    [SerializeField] private int next_level_id;
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject end_screen;
-    void Update()
-    {
-        // Moves the object forward two units every frame.
-        end_screen.transform.Translate(0, 0, 2);
-    }
-    public IEnumerator MoveToScene()
-    {
-        yield return new WaitForSeconds(3);
-        SceneManager.LoadScene(next_level_id);
-    }
-    public void MoveToScene2()
->>>>>>> Stashed changes:Assets/Scripts/scene_management/change_scene.cs
     {
         SceneLoader.Instance.LoadScene(_sceneName, true);
     }
@@ -31,11 +14,7 @@ public class NextSceneDoor : MonoBehaviour
     {
         if (collision.TryGetComponent(out PlayerManager player))
         {
-            //TransitionScreen();
-            StartCoroutine(MoveToScene());
+            MoveToScene();
         }
     }
-    //void TransitionScreen()
-    //{
-    //}
 }
