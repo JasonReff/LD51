8fde5c33130a06dc3db38eb3944bf78fd8442aff
JasonReff <jmr8382@rit.edu>
2022-10-04 14:53:48 -0400

    Post jam fixes

---
+++ Assets/Scripts/Lighting/DarknessSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DarknessSource : MonoBehaviour
+{
+    private List<ContactPoint2D> contactPoints = new List<ContactPoint2D>();
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<ToggleLight>(out ToggleLight lightToggle))
+        {
+            lightToggle.ChangeVisibility(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<ToggleLight>(out ToggleLight lightToggle))
+        {
+            lightToggle.ChangeVisibility(true);
+        }
+    }
+}
