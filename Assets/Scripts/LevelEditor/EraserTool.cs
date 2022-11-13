2f7b195fb4012e707dc9cf526d58cbb2d13b1026
JasonReff <jmr8382@rit.edu>
2022-11-13 10:13:43 -0500

    Lighting test scene

---
+++ Assets/Scripts/LevelEditor/EraserTool.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace LevelEditor
+{
+    [CreateAssetMenu(menuName = "LevelEditor/Eraser")]
+    public class EraserTool : LevelEditorTool
+    {
+        [SerializeField] private bool _eraseAllObjects;
+        private GameObject _selectedPrefab;
+        
+        public override void UseTool(Vector3Int tileCoordinates, LevelEditorTilemap tilemap)
+        {
+            base.UseTool(tileCoordinates, tilemap);
+            EraseObject(tileCoordinates, tilemap);
+        }
+
+        private void EraseObject(Vector3Int tileCoordinates, LevelEditorTilemap tilemap)
+        {
+
+            if (_eraseAllObjects)
+            {
+                tilemap.EraseGameObject(tileCoordinates);
+            }
+            else
+            {
+                tilemap.EraseGameObject(tileCoordinates, _selectedPrefab);
+            }
+        }
+    }
+}
